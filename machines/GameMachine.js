b3efc64d96f845711cee1c05e011b494935230a8
Fraser <fraser.mcintosh@emiratesteamnz.com>
2020-05-03 21:54:28 +1200

    Work on machine front/backend split

--- machines/GameMachine.js
+++ machines/GameMachine.js
@@ -52,10 +52,8 @@ const ClientMachine = Machine({
       invoke: {
         id: 'socket',
         src: (ctx, event) => (callback, onEvent) => {
-          console.log('INVOKE GAME SOCKET', ctx.gameID);
           const channel = pusherClient.subscribe(`${ctx.gameID}-host-events`);
           channel.bind('events', async (event) => {
-            console.log('GAME ', event);
             if (Array.isArray(event)) {
               event.forEach((event) => {
                 callback({ type: 'TO_PARENT', event });
@@ -66,7 +64,6 @@ const ClientMachine = Machine({
           });
 
           onEvent(async (event) => {
-            console.log('BROADCAST', event);
             pusher.trigger(`${event.gameID}-game-events`, 'events', event, (err) => {});
           });
 
@@ -78,7 +75,6 @@ const ClientMachine = Machine({
         TO_PARENT: {
           actions: [
             sendParent((ctx, event) => {
-              console.log('sent to machine, ', event, event.event);
               return event.event;
             }),
           ],
@@ -87,7 +83,6 @@ const ClientMachine = Machine({
         '*': {
           actions: send(
             (ctx, event) => {
-              console.log('SEND TO PLAYER', event);
               return {
                 ...event,
               };
@@ -141,24 +136,18 @@ const GameMachine = Machine(
               send((ctx, event) => ({ type: 'CONNECT_TO_GAME', gameID: event.gameID }), { to: 'client' }),
               assign((ctx, event) => {
                 ctx.game.players[event.playerID] = {
-                  ref: spawn(
-                    playerMachine.withContext({
-                      ...playerMachine.context,
-                      id: event.playerID,
-                      username: event.username,
-                    })
-                  ),
+                  ref: spawn(playerMachine),
                   id: event.playerID,
                   username: event.username,
                 };
               }),
-              send((ctx, event) => ({ type: 'JOIN_GAME', gameID: event.gameID, host: true }), {
-                to: (ctx, event) => ctx.game.players[event.playerID].ref,
-              }),
-              (ctx, event) => console.log('ceate game', ctx, event),
-              (ctx, event) => {
-                event.callback();
-              },
+              send(
+                (ctx, event) => ({ type: 'JOIN_GAME', gameID: event.gameID, playerID: event.playerID, host: true }),
+                {
+                  to: (ctx, event) => ctx.game.players[event.playerID].ref,
+                }
+              ),
+              'broadcastGameState',
             ],
           },
         },
@@ -171,22 +160,29 @@ const GameMachine = Machine(
             cond: 'playersFromBothTeams',
           },
           CHANGE_TEAM: {
-            actions: [
-              (ctx) => console.log('CHANGE TEAMS', ctx.game),
-              assign((ctx, event) => (ctx.game.teams[event.userID] = event.team)),
-              'broadcastGameState',
-            ],
+            actions: [assign((ctx, event) => (ctx.game.teams[event.userID] = event.team)), 'broadcastGameState'],
           },
           PLAYER_JOIN: {
             actions: [
-              log(),
               assign((ctx, event) => {
                 ctx.game.players[event.playerID] = {
-                  ref: spawn(playerMachine),
+                  ref: spawn(
+                    playerMachine.withContext({
+                      ...playerMachine.context,
+                      id: event.playerID,
+                      username: event.username,
+                    })
+                  ),
                   id: event.playerID,
                   username: event.username,
                 };
               }),
+              send(
+                (ctx, event) => ({ type: 'JOIN_GAME', gameID: event.gameID, playerID: event.playerID, host: false }),
+                {
+                  to: (ctx, event) => ctx.game.players[event.playerID].ref,
+                }
+              ),
               'broadcastGameState',
             ],
           },
@@ -204,12 +200,11 @@ const GameMachine = Machine(
             on: {
               START_TURN: {
                 target: 'preTurn',
-                actions: ['log', 'assignWord', 'setTeam', 'assignNextPlayer', 'broadcastPlayState', 'broadcastPreTurn'],
+                actions: ['assignWord', 'setTeam', 'assignNextPlayer', 'broadcastPlayState', 'broadcastPreTurn'],
               },
             },
           },
           preTurn: {
-            // entry: ['setTeam', 'assignNextPlayer', 'broadcastPlayState', 'broadcastPreTurn'],
             after: {
               15000: 'playing',
             },
@@ -249,11 +244,9 @@ const GameMachine = Machine(
         const newCurrentTeamInfo = { ...currentTeamInfo, points: currentTeamInfo.points + 1 };
         ctx.play[ctx.play.currentTeam] = newCurrentTeamInfo;
       }),
-      log: (ctx, event) => console.log(event),
       assignWord: assign((ctx, event) => (ctx.play.word = wordList[Math.floor(Math.random() * wordList.length)])),
       broadcastPoints: send(
         (ctx, event) => {
-          console.log(ctx);
           return { type: 'POINTS_UPDATE', team1: ctx.play.team1.points, team2: ctx.play.team2.points };
         },
         { to: 'client' }
@@ -264,78 +257,119 @@ const GameMachine = Machine(
         const nextPlayerIndex = lastPlayedIndex + 1 >= members.length ? 0 : lastPlayedIndex + 1;
         const nextPlayer = members[nextPlayerIndex];
         ctx.play.currentPlayer = nextPlayer;
+        console.log('assignNextPlayer', ctx, nextPlayer);
       }),
       setTeam: assign((ctx, event) => {
-        console.log('START TURN');
         ctx.play.currentTeam = ctx.play.currentTeam && ctx.play.currentTeam === 'team1' ? 'team2' : 'team1';
       }),
       cleanupTurn: assign((ctx, event) => {
         const { currentTeam, currentPlayer } = ctx.play;
         ctx.play[currentTeam].lastPlayed = currentPlayer;
       }),
-      broadcastBeforeTurn: send('BEFORE_TURN', { to: 'client' }),
-      broadcastPreTurn: send('PRE_TURN', { to: 'client' }),
-      broadcastTurn: send('TURN', { to: 'client' }),
-      broadcastEndOfTurn: send('END_OF_TURN', { to: 'client' }),
-      broadcastPlayState: send(
-        (ctx, event) => {
-          const { currentPlayer, team1, team2, currentTeam, word } = ctx.play;
-          const playerDrawing = ctx.game.players[currentPlayer].username;
-          const playerEvents = {};
-          playerEvents[currentPlayer] = {
-            type: 'DRAW',
-            word,
-          };
+      broadcastBeforeTurn: pure((context, event) => {
+        // I could in theory broadcast this to all players at once
+        // but instead I"m sending it through the player machine as an intermediary
+        return Object.values(context.game.players).map(({ ref }) => {
+          return send(
+            (ctx, event) => ({
+              type: 'BEFORE_TURN',
+            }),
+            { to: ref }
+          );
+        });
+      }),
+      broadcastPreTurn: pure((context, event) => {
+        // I could in theory broadcast this to all players at once
+        // but instead I"m sending it through the player machine as an intermediary
+        return Object.values(context.game.players).map(({ ref }) => {
+          return send(
+            (ctx, event) => ({
+              type: 'PRE_TURN',
+            }),
+            { to: ref }
+          );
+        });
+      }),
+      broadcastTurn: pure((context, event) => {
+        // I could in theory broadcast this to all players at once
+        // but instead I"m sending it through the player machine as an intermediary
+        return Object.values(context.game.players).map(({ ref }) => {
+          return send(
+            (ctx, event) => ({
+              type: 'TURN',
+            }),
+            { to: ref }
+          );
+        });
+      }),
+      broadcastEndOfTurn: pure((context, event) => {
+        // I could in theory broadcast this to all players at once
+        // but instead I"m sending it through the player machine as an intermediary
+        return Object.values(context.game.players).map(({ ref }) => {
+          return send(
+            (ctx, event) => ({
+              type: 'END_OF_TURN',
+            }),
+            { to: ref }
+          );
+        });
+      }),
+      broadcastPlayState: pure((ctx, event) => {
+        const { currentPlayer, team1, team2, currentTeam, word } = ctx.play;
+        const playerDrawing = ctx.game.players[currentPlayer].username;
 
-          const currentTeamData = currentTeam === 'team2' ? team2 : team1;
-          const otherTeamData = currentTeam === 'team2' ? team1 : team2;
-          console.log(currentTeam, currentTeamData, otherTeamData);
-          currentTeamData.members.forEach((member) => {
-            if (member !== currentPlayer) {
-              playerEvents[member] = {
-                type: 'GUESS',
-                playerDrawing: playerDrawing,
-              };
-            }
-          });
-
-          otherTeamData.members.forEach((member) => {
-            if (member !== currentPlayer) {
-              playerEvents[member] = {
-                type: 'SPECTATE',
-                playerDrawing: playerDrawing,
-                word,
-              };
-            }
-          });
-          console.log('PLAYER EVENTS', playerEvents);
-          return { type: 'PLAY_UPDATE', gameID: ctx.gameID, playerEvents };
-        },
-        { to: 'client' }
-      ),
-      broadcastGameState: send(
-        (ctx, event) => ({
-          type: 'GAME_UPDATE',
-          gameID: ctx.gameID,
-          game: ctx.game,
-        }),
-        { to: 'client' }
-      ),
+        const currentTeamData = currentTeam === 'team2' ? team2 : team1;
+        const otherTeamData = currentTeam === 'team2' ? team1 : team2;
+        return Object.values(ctx.game.players).map(({ ref, id }) => {
+          let playerEvent;
+          if (id === currentPlayer) {
+            playerEvent = {
+              type: 'DRAW',
+              word,
+            };
+          } else if (currentTeamData.members.includes(id)) {
+            playerEvent = {
+              type: 'GUESS',
+              playerDrawing: playerDrawing,
+            };
+          } else {
+            playerEvent = {
+              type: 'SPECTATE',
+              playerDrawing: playerDrawing,
+              word,
+            };
+          }
+          return send((ctx, event) => ({ ...playerEvent, gameID: ctx.gameID }), { to: ref });
+        });
+      }),
+      broadcastGameState: pure((context, event) => {
+        // I could in theory broadcast this to all players at once
+        // but instead I"m sending it through the player machine as an intermediary
+        return Object.values(context.game.players).map(({ ref }) => {
+          return send(
+            (ctx, event) => ({
+              type: 'GAME_UPDATE',
+              gameID: ctx.gameID,
+              game: ctx.game,
+            }),
+            { to: ref }
+          );
+        });
+      }),
       broadcastStartGame: pure((context, event) => {
         // I could in theory broadcast this to all players at once
         // but instead I"m sending it through the player machine as an intermediary
         return Object.values(context.game.players).map(({ ref }) => {
-          return send('GAME_UPATE', { to: ref });
+          return send(
+            (ctx, event) => ({
+              type: 'START_GAME',
+              gameID: ctx.gameID,
+              game: ctx.game,
+            }),
+            { to: ref }
+          );
         });
       }),
-      broadcastStartGame: send(
-        (ctx, event) => ({
-          type: 'START_GAME',
-          gameID: ctx.gameID,
-          game: ctx.game,
-        }),
-        { to: 'client' }
-      ),
       derivePlayState: assign((ctx, event) => {
         ctx.play = {
           team1: {
--- machines/PlayerMachine.js
+++ machines/PlayerMachine.js
@@ -1,6 +1,7 @@
 const { actions, Machine, send, sendParent, spawn } = require('xstate');
 const { v4: uuid } = require('uuid');
 const { assign } = require('@xstate/immer');
+const { log } = actions;
 
 var PusherClient = require('pusher-js');
 const fetch = require('node-fetch');
@@ -31,12 +32,16 @@ const ClientMachine = Machine({
   states: {
     idle: {
       on: {
-        CONNECT_TO_GAME: {
+        JOIN_GAME: {
           target: 'connected',
-          actions: assign((ctx, event) => {
-            ctx.gameID = event.gameID;
-            ctx.playerID = event.playerID;
-          }),
+
+          actions: [
+            assign((ctx, event) => {
+              ctx.gameID = event.gameID;
+              ctx.playerID = event.playerID;
+            }),
+            (ctx, event) => console.log(event),
+          ],
         },
       },
     },
@@ -45,7 +50,7 @@ const ClientMachine = Machine({
         id: 'socket',
         src: (context, event) => (callback, onEvent) => {
           const channel = pusherClient.subscribe(`${context.gameID}-${context.playerID}-player-events`);
-          console.log('client engaged');
+          console.log('client engaged', `${context.gameID}-${context.playerID}-player-events`);
           channel.bind('remoteEvents', async (event) => {
             console.log('RECIEVED FROM LOCAL PLAYER', event);
             if (Array.isArray(event)) {
@@ -87,232 +92,4 @@ const ClientMachine = Machine({
   },
 });
 
-const PlayerMachine = Machine({
-  id: 'player',
-  initial: 'ready',
-  context: {
-    id: uuid(),
-    username: undefined,
-    team: undefined,
-    gameID: undefined,
-    host: false,
-    game: {
-      players: [],
-      teams: {},
-    },
-    play: {
-      word: undefined,
-    },
-    points: {
-      team1: 0,
-      team2: 0,
-    },
-    preTurn: {
-      countdown: 15,
-      duration: 15,
-      interval: 1,
-    },
-    turn: {
-      countdown: 60,
-      duration: 60,
-      interval: 1,
-    },
-  },
-  invoke: {
-    id: 'client',
-    src: ClientMachine,
-  },
-  states: {
-    ready: {
-      on: {
-        UPDATE_USERNAME: {
-          actions: assign((ctx, event) => (ctx.username = event.username)),
-        },
-        JOIN_GAME: {
-          target: 'lobby',
-          actions: [
-            assign((ctx, event) => {
-              ctx.gameID = event.gameID;
-              ctx.host = event.host;
-            }),
-            send((ctx, event) => ({ type: 'CONNECT_TO_GAME', gameID: event.gameID, playerID: ctx.id }), {
-              to: 'client',
-            }),
-            (ctx) => console.log('YODSUFOISDUFO', ctx),
-            send(
-              (ctx, event) => ({
-                type: 'PLAYER_STATE_UPDATE',
-                player: {
-                  host: ctx.host,
-                  state: 'lobby',
-                },
-                game: ctx.game,
-              }),
-              { to: 'client' }
-            ),
-          ],
-        },
-      },
-    },
-    lobby: {
-      on: {
-        START_GAME: {
-          target: 'playing',
-          actions: [assign((ctx, event) => (ctx.game = event.game))],
-        },
-        CHANGE_TEAM: {
-          actions: [sendParent((ctx, event) => ({ ...event, gameID: ctx.gameID, type: 'CHANGE_TEAM' }))],
-        },
-        GAME_UPDATE: {
-          actions: [
-            send(
-              (ctx, event) => ({
-                type: 'PLAYER_STATE_UPDATE',
-                player: {
-                  host: ctx.host,
-                  state: 'lobby',
-                },
-                game: event.game,
-              }),
-              { to: 'client' }
-            ),
-          ],
-        },
-      },
-    },
-    playing: {
-      type: 'parallel',
-      states: {
-        task: {
-          initial: 'idle',
-          states: {
-            idle: {},
-            drawing: {
-              entry: [() => console.log('IM In drawing state')],
-            },
-            guessing: {},
-            spectating: {},
-          },
-          on: {
-            PLAY_UPDATE: {
-              actions: [
-                send((ctx, event) => {
-                  console.log('PLAY UPDATE', event);
-                  return event.playerEvents[ctx.id];
-                }),
-              ],
-            },
-            DRAW: {
-              target: '.drawing',
-              actions: [assign((ctx, event) => (ctx.play.word = event.word))],
-            },
-            GUESS: {
-              target: '.guessing',
-              actions: [assign((ctx, event) => (ctx.play.playerDrawing = event.playerDrawing))],
-            },
-            SPECTATE: {
-              target: '.spectating',
-              actions: [
-                assign((ctx, event) => {
-                  ctx.play.word = event.word;
-                  ctx.play.playerDrawing = event.playerDrawing;
-                }),
-              ],
-            },
-          },
-        },
-        turn: {
-          initial: 'beforeTurn',
-          states: {
-            beforeTurn: {
-              on: {
-                POINTS_UPDATE: {
-                  actions: assign((ctx, event) => {
-                    ctx.points.team1 = event.team1;
-                    ctx.points.team2 = event.team2;
-                  }),
-                },
-                PRE_TURN: {
-                  target: 'preTurn',
-                },
-              },
-            },
-            preTurn: {
-              invoke: {
-                src: (context) => (cb) => {
-                  const interval = setInterval(() => {
-                    cb('TICK');
-                  }, 1000 * context.preTurn.interval);
-
-                  return () => {
-                    clearInterval(interval);
-                  };
-                },
-              },
-              on: {
-                TICK: {
-                  actions: assign((ctx, event) => {
-                    ctx.preTurn.countdown = ctx.preTurn.countdown - ctx.preTurn.interval;
-                  }),
-                },
-                TURN: {
-                  target: 'inTurn',
-                  actions: assign((ctx, event) => {
-                    ctx.preTurn.countdown = ctx.preTurn.duration;
-                  }),
-                },
-              },
-            },
-            inTurn: {
-              invoke: {
-                src: (context) => (cb) => {
-                  const interval = setInterval(() => {
-                    cb('TICK');
-                  }, 1000 * context.turn.interval);
-
-                  return () => {
-                    clearInterval(interval);
-                  };
-                },
-              },
-              on: {
-                TICK: {
-                  actions: assign((ctx, event) => {
-                    ctx.turn.countdown = ctx.turn.countdown - ctx.turn.interval;
-                  }),
-                },
-                END_OF_TURN: {
-                  target: 'endOfTurn',
-                  actions: assign((ctx, event) => {
-                    ctx.turn.countdown = ctx.turn.duration;
-                  }),
-                },
-              },
-            },
-            endOfTurn: {
-              on: {
-                BEFORE_TURN: {
-                  target: 'beforeTurn',
-                },
-              },
-            },
-          },
-          on: {
-            BEFORE_TURN: {
-              target: '.beforeTurn',
-            },
-            PRE_TURN: {
-              target: '.preTurn',
-            },
-            TURN: {
-              target: '.inTurn',
-            },
-          },
-        },
-      },
-    },
-  },
-  actions: {},
-});
-
-exports.playerMachine = PlayerMachine;
+exports.playerMachine = ClientMachine;
