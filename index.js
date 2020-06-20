c0dd86be6ac06fdfceb9fe84a2c881e14adbe41c
Fraser <fraser.mcintosh@emiratesteamnz.com>
2020-06-20 23:55:06 +1200

    Add socket io

--- index.js
+++ index.js
@@ -7,25 +7,36 @@ const { gameManagerMachine } = require('./machines/GameManagerMachine');
 const { Machine, interpret } = require('xstate');
 var cors = require('cors');
 var bodyParser = require('body-parser');
-app.use(cors());
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('EVENT', (event) => {
+  console.log('a user event', event);
+});
+
 const service = interpret(gameManagerMachine)
   .onTransition((state) => {
     console.log('service', state.value);
   })
   .start();
 
-// Send events
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.post('/game', (req, res) => {
-  service.send({ ...req.body, res });
+io.on('connection', (socket) => {
+  socket.on('event', (event) => {
+    console.log('a user event', event);
+    service.send(event);
+  });
+  console.log('connected');
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// Send events
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.post('/game', (req, res) => {
+//   service.send({ ...req.body, res });
+// });
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+http.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
